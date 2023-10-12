fb8d592c4964fdd0cb45461df42811a27d79d4d3
Dmitriy Nikolaev <dnikolaev@tradingview.com>
2023-10-12 16:34:54 +0300

    Update api client

--- api/api_client.py
+++ api/api_client.py
@@ -14,44 +14,41 @@ class ApiClient(Client):
     _TIMEOUT = 10
 
     def __init__(self) -> None:
+        self.client = httpx.Client()
         self.response = None
 
     # @allure.step("Sending a GET request to \"{self._BASE_URL}\"")
     def get(self, endpoint: str, params: dict = None, headers: dict = None) -> Response:
-        with httpx.Client() as client:
-            self.response = client.get(url=f"{self._BASE_URL}{endpoint}",
-                                       params=params,
-                                       headers=headers,
-                                       timeout=self._TIMEOUT)
+        self.response = self.client.get(url=f"{self._BASE_URL}{endpoint}",
+                                    params=params,
+                                    headers=headers,
+                                    timeout=self._TIMEOUT)
         log(response=self.response)
         return self.response
 
     # @allure.step("Sending a POST request to \"{self._BASE_URL}\"")
     def post(self, endpoint: str, data: dict = None, headers: dict = None) -> Response:
-        with httpx.Client() as client:
-            self.response = client.post(url=f"{self._BASE_URL}{endpoint}",
-                                        json=data,
-                                        headers=headers,
-                                        timeout=self._TIMEOUT)
+        self.response = self.client.post(url=f"{self._BASE_URL}{endpoint}",
+                                    json=data,
+                                    headers=headers,
+                                    timeout=self._TIMEOUT)
         log(response=self.response, request_body=data)
         return self.response
 
     # @allure.step("Sending a PUT request to \"{self._BASE_URL}\"")
     def put(self, endpoint: str, data: dict = None, headers: dict = None) -> Response:
-        with httpx.Client() as client:
-            self.response = client.put(url=f"{self._BASE_URL}{endpoint}",
-                                       json=data,
-                                       headers=headers,
-                                       timeout=self._TIMEOUT)
+        self.response = self.client.put(url=f"{self._BASE_URL}{endpoint}",
+                                    json=data,
+                                    headers=headers,
+                                    timeout=self._TIMEOUT)
         log(response=self.response, request_body=data)
         return self.response
 
     # @allure.step("Sending a DELETE request to \"{self._BASE_URL}\"")
     def delete(self, endpoint: str, params: str = None, headers: dict = None) -> Response:
-        with httpx.Client() as client:
-            self.response = client.get(url=f"{self._BASE_URL}{endpoint}",
-                                       params=params,
-                                       headers=headers,
-                                       timeout=self._TIMEOUT)
+        self.response = self.client.get(url=f"{self._BASE_URL}{endpoint}",
+                                    params=params,
+                                    headers=headers,
+                                    timeout=self._TIMEOUT)
         log(response=self.response)
         return self.response
