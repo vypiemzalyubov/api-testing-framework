88f86028dd2290f1e106bb96b6f983cdd97b1717
vypiemzalyubov <dima.rza90@mail.ru>
2023-10-16 19:46:22 +0300

    Update api client

--- api/api_client.py
+++ api/api_client.py
@@ -10,37 +10,41 @@ class ApiClient(Client):
         super().__init__(base_url=base_settings.api_url)
 
     @allure.step(f"Sending a GET request to \"{Client.base_url}\"")
-    def get(self, endpoint: str, params: dict = None, headers: dict = None, timeout: int = None) -> Response:
+    def get(self, endpoint: str, params: dict = None, headers: dict = None, cookies: dict = None, timeout: int = None) -> Response:
         self.response = super().get(url=endpoint,
                                     params=params,
                                     headers=headers,
+                                    cookies=cookies,
                                     timeout=timeout)
         log(response=self.response)
         return self.response
 
     @allure.step(f"Sending a POST request to \"{Client.base_url}\"")
-    def post(self, endpoint: str, json: dict = None, headers: dict = None, timeout: int = None) -> Response:
+    def post(self, endpoint: str, json: dict = None, headers: dict = None, cookies: dict = None, timeout: int = None) -> Response:
         self.response = super().post(url=endpoint,
                                      json=json,
                                      headers=headers,
+                                     cookies=cookies,
                                      timeout=timeout)
         log(response=self.response, request_body=json)
         return self.response
 
     @allure.step(f"Sending a PUT request to \"{Client.base_url}\"")
-    def put(self, endpoint: str, json: dict = None, headers: dict = None, timeout: int = None) -> Response:
+    def put(self, endpoint: str, json: dict = None, headers: dict = None, cookies: dict = None, timeout: int = None) -> Response:
         self.response = super().put(url=endpoint,
                                     json=json,
                                     headers=headers,
+                                    cookies=cookies,
                                     timeout=timeout)
         log(response=self.response, request_body=json)
         return self.response
 
     @allure.step(f"Sending a DELETE request to \"{Client.base_url}\"")
-    def delete(self, endpoint: str, params: str = None, headers: dict = None, timeout: int = None) -> Response:
+    def delete(self, endpoint: str, params: str = None, headers: dict = None, cookies: dict = None, timeout: int = None) -> Response:
         self.response = super().delete(url=endpoint,
                                        params=params,
                                        headers=headers,
+                                       cookies=cookies,
                                        timeout=timeout)
         log(response=self.response)
         return self.response
